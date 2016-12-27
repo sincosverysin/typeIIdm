3a5e74bf43d3772486ccc4a42b6dd3d97db309dd
sincosverysin <sincosverysin@gmail.com>
2016-12-27 20:45:04 +0800

    table 2 graph

--- 3.py
+++ 3.py
@@ -1,8 +1,15 @@
+data=[]
 for line in open("dm2_topo.csv").readlines():
    # print "-1"
    # print type(line.split(",")[0])
-    line.split(",")
-
-def find_neighbors(id):
-    if id in range(1,42):
+    li = []
+    for number in line.split(","):
+        if number.isdigit():
+            li.append(int(float(number)))
+        else:
+            li.append(number)
+    data.append(li)
+print data
+#def find_neighbors(id):
+ #   if id in range(1,42):
 
